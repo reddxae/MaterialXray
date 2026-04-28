4e552541a72996203b068bc4ac208e0d439a433f
Ivan <reddxae@proton.me>
2026-04-28 18:57:34 +0300

    chore: bump to 1.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,8 +50,8 @@ android {
         applicationId = "com.materialxray"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.2.0"
+        versionCode = 4
+        versionName = "1.3.0"
     }
 
     signingConfigs {

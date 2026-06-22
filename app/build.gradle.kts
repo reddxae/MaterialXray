3f898c8cfd74e2a75173643b7554f6c5a02db8f9
Aether <aether@pierdoling.org>
2026-06-23 00:14:26 +0300

    chore: bump to 0.3.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,8 +54,8 @@ android {
         applicationId = "com.material.xray"
         minSdk = 28
         targetSdk = 36
-        versionCode = 301
-        versionName = "0.3.1"
+        versionCode = 302
+        versionName = "0.3.2"
 
         ndk {
             abiFilters += "arm64-v8a"

94468b167c6dbfcd52dd5f85dafa76d1d1b16cf1
Aether <aether@pierdoling.org>
2026-07-28 22:40:57 +0300

    chore: bump to 0.5.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,8 +57,8 @@ android {
         applicationId = "com.material.xray"
         minSdk = 28
         targetSdk = 36
-        versionCode = 550
-        versionName = "0.5.5"
+        versionCode = 560
+        versionName = "0.5.6"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         ndk {
